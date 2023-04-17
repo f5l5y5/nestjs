8aee386207bef4f7240b6d9faefb5a008b661886
f5l5y5 <1273179133@qq.com>
2023-04-18 06:37:57 +0800

    feat: 数据库连接

---
+++ database/src/test/entities/test.entity.ts
@@ -0,0 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Test {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  name: string;
+
+  @Column()
+  age: number;
+
+  @Column()
+  password: number;
+}
