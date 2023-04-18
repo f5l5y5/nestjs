31ff828173e628655af7612a8b7b76186866ce60
fuyunlong <1273179133@qq.com>
2023-04-18 13:37:16 +0800

    feat: 数据库增删改查

--- database/src/test/test.service.ts
+++ database/src/test/test.service.ts
@@ -1,26 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTestDto } from './dto/create-test.dto';
 import { UpdateTestDto } from './dto/update-test.dto';
-
+import { FindOneOptions, Repository } from 'typeorm';
+import { Test } from './entities/test.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 @Injectable()
 export class TestService {
-  create(createTestDto: CreateTestDto) {
-    return 'This action adds a new test';
+  constructor(
+    @InjectRepository(Test) private readonly testRepository: Repository<Test>,
+  ) {}
+
+  async create(createTestDto: CreateTestDto) {
+    console.log('打印***createTestDto', createTestDto);
+    console.log('打印***this.', this.testRepository);
+    const newUser = await this.testRepository.create(createTestDto);
+    return await this.testRepository.save(newUser);
   }
 
-  findAll() {
-    return `This action returns all test`;
+  async findAll() {
+    return await this.testRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} test`;
+  async findOne(id: number) {
+    return await this.testRepository.findOne({ where: { id } });
   }
 
-  update(id: number, updateTestDto: UpdateTestDto) {
-    return `This action updates a #${id} test`;
+  async update(id: number, updateTestDto: UpdateTestDto) {
+    const updateUser = await this.testRepository.findOne({ where: { id } });
+    if (!updateUser) {
+      throw new Error(`User with id ${id} not found.`);
+    }
+    await this.testRepository.merge(updateUser, updateTestDto);
+    return await this.testRepository.save(updateUser);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} test`;
+  async remove(id: number) {
+    console.log('打印***1', 1);
+    const userToRemove = await this.testRepository.findOneOrFail({
+      where: { id },
+    });
+    if (!userToRemove) {
+      throw new Error(`User with id ${id} not found.`);
+    }
+    return await this.testRepository.remove(userToRemove);
   }
 }
